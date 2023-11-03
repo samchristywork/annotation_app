933fc6e6265c2f13f48bebd86e1c5efc75431853
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-03 15:24:00 -0500

    feat: Add a function to find the location of a user selection
    This is tricker than it seems because we want to find the location
    within the text, not within the DOM.

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ var root = document.getElementById("root");
 var body_text = "";
 text_area.value = body_text;
 
+function getSelectionCharacterOffsetWithin(element) {
+  var start = 0;
+  var end = 0;
+  var sel, range, priorRange;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+      priorRange = range.cloneRange();
+      priorRange.selectNodeContents(element);
+      priorRange.setEnd(range.startContainer, range.startOffset);
+      start = priorRange.toString().length;
+      priorRange.setEnd(range.endContainer, range.endOffset);
+      end = priorRange.toString().length;
+    }
+  }
+  return { start: start, end: end };
+}
+
 function render() {
   let html = '';
   html += '<div class="annotated_text">';
